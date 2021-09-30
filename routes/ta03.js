14493cfe44fab339bd989c2251ac8903f167a79a
Kim Black <kimblackaz@gmail.com>
2021-09-30 11:03:18 -0600

    Pretty
    Co-authored-by: Cierra M <CierraM@users.noreply.github.com>
    Co-authored-by: joseaquinof <joseaquinof@users.noreply.github.com>
    Co-authored-by: Christian <chrismarti343@users.noreply.github.com>

--- routes/ta03.js
+++ routes/ta03.js
@@ -2,12 +2,33 @@
 const express = require('express');
 const router = express.Router();
 
+const products = require('../data/products.json')
+
+router.get('/search', (req, res, next) => {
+  const tag = req.query.tag;
+  if (tag == "") {
+    res.render('pages/ta03', {
+      title: 'Team Activity 03',
+      path: '/ta03',
+      products: []
+    })
+  }
+  console.log(tag);
+  let caseTag = tag[0].toUpperCase() + tag.substring(1).toLowerCase();
+  console.log(caseTag);
+  const updatedProducts = products.filter(products => products.tags.includes(caseTag));
+  res.render('pages/ta03', {
+    title: 'Team Activity 03',
+    path: '/ta03',
+    products: updatedProducts
+  })
+});
+
 router.get('/', (req, res, next) => {
   res.render('pages/ta03', {
     title: 'Team Activity 03',
     path: '/ta03', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
+    products: products
   });
 });
 
